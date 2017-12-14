2035351d86b30374983aa8c77d7d367022c83e9a
savaughn <savaughn@uark.edu>
2017-12-14 01:07:19 -0600

    LCD blue screen test

---
+++ ArduinoTimer_TFT.ino.ino
@@ -0,0 +1,38 @@
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+#include <SPI.h>          // f.k. for Arduino-1.5.2
+#include "Adafruit_GFX.h"// Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+
+MCUFRIEND_kbv tft;
+// Assign human-readable names to some common 16-bit color values:
+#define    BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+int width = tft.width();
+  int height = tft.height(); 
+
+void setup() {
+  int ID = tft.readID();
+  tft.begin(ID);
+  tft.setRotation(0);
+  tft.fillScreen(WHITE);
+  
+
+}
+
+void loop() {
+ 
+  tft.fillRect(0,0,width, height, BLUE);
+
+}
